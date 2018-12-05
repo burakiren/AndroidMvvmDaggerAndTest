68523d9be175c0927df0fa7e53148078a97e40d8
burak.iren <burakiren@hotmail.com.tr>
2018-12-05 14:59:22 +0300

    activity injector

--- app/src/main/java/com/burakiren/androidmvvmdaggerandtest/di/ActivityInjector.java
+++ app/src/main/java/com/burakiren/androidmvvmdaggerandtest/di/ActivityInjector.java
@@ -2,6 +2,8 @@ package com.burakiren.androidmvvmdaggerandtest.di;
 
 import android.app.Activity;
 
+import com.burakiren.androidmvvmdaggerandtest.base.BaseActivity;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,5 +24,23 @@ public class ActivityInjector {
     @Inject
     ActivityInjector(Map<Class<? extends Activity>, Provider<AndroidInjector.Factory<? extends Activity>>> activityInjectors) {
         this.activityInjectors = activityInjectors;
+    }
+
+    void inject(Activity activity) {
+
+        if (!(activity instanceof BaseActivity)) {
+            throw new IllegalArgumentException("Activity must extend BaseActivity");
+        }
+
+        String instanceId = ((BaseActivity) activity).getInstanceId();
+        if (cache.containsKey(instanceId)) {
+            //noinspection unchecked
+            ((AndroidInjector<Activity>) cache.get(instanceId)).inject(activity);
+            return;
+        }
+
+        //noinspection unchecked
+        AndroidInjector.Factory<Activity> injectorFactory = (AndroidInjector.Factory<Activity>) activityInjectors.get(activity.getClass()).get();
+        AndroidInjector<Activity> activityAndroidInjector = injectorFactory.create(activity);
     }
 }
